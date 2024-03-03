0b138a0f20da3d1b87c98fa78171be3d9f84149b
Marcos Sebastian Portales Ramos <101372403+marcosportales@users.noreply.github.com>
2024-03-02 19:13:28 -0500

    feat: add wifi ip cubit

--- lib/number_translator/presentation/bloc/configurations/configurations_cubit.dart
+++ lib/number_translator/presentation/bloc/configurations/configurations_cubit.dart
@@ -1,8 +1,15 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 
 part 'configurations_state.dart';
 
 class ConfigurationsCubit extends Cubit<ConfigurationsState> {
   ConfigurationsCubit() : super(const ConfigurationsInitial());
+
+  void getIP() async {
+    emit(state.copyWith(wifiIp: await NetworkInfo().getWifiIP()));
+  }
 }
--- lib/number_translator/presentation/bloc/configurations/configurations_state.dart
+++ lib/number_translator/presentation/bloc/configurations/configurations_state.dart
@@ -2,24 +2,23 @@ part of 'configurations_cubit.dart';
 
 abstract class ConfigurationsState extends Equatable {
   const ConfigurationsState();
+
+  ConfigurationsInitial copyWith({String? baseUrl, String? wifiIp});
 }
 
 class ConfigurationsInitial extends ConfigurationsState {
   final String baseUrl;
+  final String wifiIp;
 
-  const ConfigurationsInitial({
-    this.baseUrl = '0.0.0.0'
-  });
-  
+  const ConfigurationsInitial(
+      {this.baseUrl = '0.0.0.0', this.wifiIp = '192.168.43.9'});
+
   @override
-  ConfigurationsInitial copyWith({
-    String? baseUrl
-  }) {
+  ConfigurationsInitial copyWith({String? baseUrl, String? wifiIp}) {
     return ConfigurationsInitial(
-        baseUrl: baseUrl ?? this.baseUrl
-    );
-}
+        baseUrl: baseUrl ?? this.baseUrl, wifiIp: wifiIp ?? this.wifiIp);
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [baseUrl, wifiIp];
 }
