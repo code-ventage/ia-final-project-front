79ec43d23d88b831eccba4f31921714358753dd0
palmerodev <victor.palmero@undostres.com.mx>
2024-03-13 19:05:13 -0400

    feat: implement methods calls

--- lib/number_translator/data/data_sources/auth/auth_datasource.dart
+++ lib/number_translator/data/data_sources/auth/auth_datasource.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:ia_final_project_front/number_translator/data/models/user_model.dart';
+
+import '../../../../config/config_data/configuration_data.dart';
+import '../../../../config/connection/connection_helper.dart';
+import '../../../../config/service_locator/service_locator.dart';
 
 abstract class AuthDatasource {
   Future<bool> signUp(UserModel userModel);
@@ -7,15 +12,74 @@ abstract class AuthDatasource {
 }
 
 class AuthDatasourceImpl extends AuthDatasource {
+
+  final ConnectionHelper connectionHelper;
+
+  AuthDatasourceImpl({required this.connectionHelper});
+
   @override
   Future<bool> logIn(UserModel userModel) async{
-    // TODO: implement logIn
-    throw UnimplementedError();
+    var response = <String, dynamic>{};
+    try {
+      if (serviceLocator.get<ConfigurationData>().DEBUGING) {
+        response = {
+          "version": "0",
+          "response": {
+            "status": "200",
+            "message": "",
+            "data": [
+              {
+                "username": "pepe",
+                "password": "123123"
+              }
+            ]
+          }
+        };
+        return response['response']['status'] == '200';
+      }
+      response = (await connectionHelper.dio.post(
+        '/user/login',
+        data: userModel.toJson(),
+      ))
+          .data;
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+
+    return response['response']['status'] == '200';
   }
 
   @override
   Future<bool> signUp(UserModel userModel) async{
-    // TODO: implement signUp
-    throw UnimplementedError();
+    var response = <String, dynamic>{};
+    try {
+      if (serviceLocator.get<ConfigurationData>().DEBUGING) {
+        response = {
+          "version": "0",
+          "response": {
+            "status": "200",
+            "message": "",
+            "data": [
+              {
+                "username": "pepe",
+                "password": "123123"
+              }
+            ]
+          }
+        };
+        return response['response']['status'] == '200';
+      }
+      response = (await connectionHelper.dio.post(
+        '/user/signup',
+        data: userModel.toJson(),
+      ))
+          .data;
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+
+    return response['response']['status'] == '200';
   }
 }
