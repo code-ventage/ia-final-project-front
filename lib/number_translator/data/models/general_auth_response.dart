69658e7efa4b4a8d6c84f4cb431218b1ca412849
palmerodev <victor.palmero@undostres.com.mx>
2024-03-13 18:51:25 -0400

    feat: implement general response

--- lib/number_translator/data/models/general_auth_response.dart
+++ lib/number_translator/data/models/general_auth_response.dart
@@ -1,3 +1,57 @@
-class GeneralAuthResponse {
-  // todo implement this
+class GeneralResponse<T> {
+  final String version;
+  final Response<T> response;
+
+  GeneralResponse({required this.version, required this.response});
+
+  factory GeneralResponse.fromJson(Map<String, dynamic> json) {
+    return GeneralResponse(
+      version: json['version'] ?? '',
+      response: Response.fromJson(json['response']),
+    );
+  }
+}
+
+class Response<T> {
+  final String status;
+  final String message;
+  final List<T> data;
+
+  Response({required this.status, required this.message, required this.data});
+
+  factory Response.fromJson(Map<String, dynamic> json) {
+    return Response(
+      status: json['status'] ?? '',
+      message: json['message'] ?? '',
+      data: List.from((json['data'] as List).map((e) => (T is UserScoreResponse) ? UserScoreResponse.fromJson(e) : UserResponse.fromJson(e))),
+    );
+  }
+}
+
+class UserResponse {
+  final String username;
+  final String password;
+
+  UserResponse({required this.username, required this.password});
+
+  factory UserResponse.fromJson(Map<String, dynamic> json) {
+    return UserResponse(
+      username: json['username'] ?? '',
+      password: json['password'] ?? '',
+    );
+  }
+}
+
+class UserScoreResponse {
+  final String username;
+  final String score;
+
+  UserScoreResponse({required this.username, required this.score});
+
+  factory UserScoreResponse.fromJson(Map<String, dynamic> json) {
+    return UserScoreResponse(
+      username: json['username'] ?? '',
+      score: json['score'] ?? '',
+    );
+  }
 }
