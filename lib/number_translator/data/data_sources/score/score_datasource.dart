f97428df8bb14000ac70d981739cacf6b06a182b
palmerodev <victor.palmero@undostres.com.mx>
2024-03-13 19:00:17 -0400

    feat: implement getAll and save calls

--- lib/number_translator/data/data_sources/score/score_datasource.dart
+++ lib/number_translator/data/data_sources/score/score_datasource.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ia_final_project_front/number_translator/data/models/general_users_response.dart';
 import 'package:ia_final_project_front/number_translator/data/models/user_score_model.dart';
+
+import '../../../../config/config_data/configuration_data.dart';
+import '../../../../config/connection/connection_helper.dart';
+import '../../../../config/service_locator/service_locator.dart';
 
 abstract class ScoreDatasource {
   Future<bool> save(UserScoreModel userModel);
@@ -7,15 +13,80 @@ abstract class ScoreDatasource {
 }
 
 class ScoreDatasourceImpl extends ScoreDatasource {
+
+  final ConnectionHelper connectionHelper;
+
+  ScoreDatasourceImpl({required this.connectionHelper});
   @override
   Future<List<UserScoreModel>> getAll() async{
-    // TODO: implement getAll
-    throw UnimplementedError();
+    var response = <String, dynamic>{};
+    try {
+      if (serviceLocator.get<ConfigurationData>().DEBUGING) {
+        response = {
+          "version": "0",
+          "response": {
+            "status": "200",
+            "message": "",
+            "data": [
+              {
+                "username": "pepe",
+                "score": "123123"
+              }
+            ]
+          }
+        };
+        return GeneralUsersResponse<UserScoreResponse>.fromJson(response).response.data.map((e) => UserScoreModel(
+          username: e.username,
+          score: e.score,
+        )).toList();
+      }
+      debugPrint('baseUrl: ${connectionHelper.dio.options.baseUrl}');
+      response = (await connectionHelper.dio.get(
+        '/user/score/getAll',
+      ))
+          .data;
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return [];
+    }
+
+    return GeneralUsersResponse<UserScoreResponse>.fromJson(response).response.data.map((e) => UserScoreModel(
+      username: e.username,
+      score: e.score,
+    )).toList();
   }
 
   @override
   Future<bool> save(UserScoreModel userModel) async{
-    // TODO: implement save
-    throw UnimplementedError();
+    var response = <String, dynamic>{};
+    try {
+      if (serviceLocator.get<ConfigurationData>().DEBUGING) {
+        response = {
+          "version": "0",
+          "response": {
+            "status": "200",
+            "message": "",
+            "data": [
+              {
+                "username": "pepe",
+                "score": "123123"
+              }
+            ]
+          }
+        };
+        return GeneralUsersResponse<UserScoreResponse>.fromJson(response).response.status == '200';
+      }
+      debugPrint('baseUrl: ${connectionHelper.dio.options.baseUrl}');
+      response = (await connectionHelper.dio.post(
+        '/user/score/store',
+        data: userModel.toJson(),
+      ))
+          .data;
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+
+    return GeneralUsersResponse<UserScoreResponse>.fromJson(response).response.status == '200';
   }
 }
