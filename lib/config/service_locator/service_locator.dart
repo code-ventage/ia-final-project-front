57b56041f07fdab817151e6bf999869ced819a40
palmerodev <victor.palmero@undostres.com.mx>
2024-03-18 17:46:20 -0400

    feat: add initializations to servicelocator

--- lib/config/service_locator/service_locator.dart
+++ lib/config/service_locator/service_locator.dart
@@ -1,8 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:ia_final_project_front/config/config_data/configuration_data.dart';
 import 'package:ia_final_project_front/config/connection/connection_helper.dart';
+import 'package:ia_final_project_front/number_translator/data/data_sources/auth/auth_datasource.dart';
+import 'package:ia_final_project_front/number_translator/data/repositories/auth/auth_repository.dart';
 import 'package:ia_final_project_front/number_translator/data/repositories/number_translator/number_translator_repository.dart';
+import 'package:ia_final_project_front/number_translator/domain/repositories/auth/auth_domain_repository.dart';
+import 'package:ia_final_project_front/number_translator/domain/use_cases/auth/auth_service.dart';
 import 'package:ia_final_project_front/number_translator/domain/use_cases/number_translator/number_translator_service.dart';
+import 'package:ia_final_project_front/number_translator/presentation/bloc/auth/auth_cubit.dart';
 import 'package:ia_final_project_front/number_translator/presentation/bloc/configurations/configurations_cubit.dart';
 import 'package:ia_final_project_front/number_translator/presentation/bloc/translation/number_translator_cubit.dart';
 import 'package:ia_final_project_front/number_translator/presentation/pages/configurations/configurations_page.dart';
@@ -20,23 +25,22 @@ final serviceLocator = GetIt.instance;
 void setupServiceLocator() {
   serviceLocator.registerSingleton<ConfigurationData>(ConfigurationData());
   serviceLocator.registerSingleton<ConnectionHelper>(ConnectionHelper());
-  serviceLocator
-      .registerSingleton<NumberTranslatorCubit>(NumberTranslatorCubit());
-  serviceLocator.registerSingleton<ConfigurationsCubit>(ConfigurationsCubit());
-  serviceLocator.registerSingleton<GameCubit>(GameCubit());
-  serviceLocator.registerSingleton<ThemeSelectorCubit>(ThemeSelectorCubit());
 
   setupDataServices();
   setupDomainServices();
+
+  serviceLocator.registerSingleton<NumberTranslatorCubit>(NumberTranslatorCubit());
+  serviceLocator.registerSingleton<ConfigurationsCubit>(ConfigurationsCubit());
+  serviceLocator.registerSingleton<GameCubit>(GameCubit());
+  serviceLocator.registerSingleton<ThemeSelectorCubit>(ThemeSelectorCubit());
+  serviceLocator.registerSingleton<AuthCubit>(AuthCubit());
 
   setupPages();
 }
 
 void setupPages() {
-  serviceLocator
-      .registerSingleton<NumberTranslatorPage>(const NumberTranslatorPage());
-  serviceLocator
-      .registerSingleton<ConfigurationsPage>(const ConfigurationsPage());
+  serviceLocator.registerSingleton<NumberTranslatorPage>(const NumberTranslatorPage());
+  serviceLocator.registerSingleton<ConfigurationsPage>(const ConfigurationsPage());
   serviceLocator.registerSingleton<GamePage>(const GamePage());
   serviceLocator.registerSingleton<AuthPage>(const AuthPage());
 }
@@ -49,20 +53,34 @@ void setupDataServices() {
   );
 
   serviceLocator.registerSingleton<NumberTranslatorRepository>(
-    NumberTranslatorRepositoryImpl(
-        datasource: serviceLocator.get<NumberTranslatorDatasource>()),
+    NumberTranslatorRepositoryImpl(datasource: serviceLocator.get<NumberTranslatorDatasource>()),
+  );
+
+  serviceLocator.registerSingleton<AuthDatasource>(
+    AuthDatasourceImpl(connectionHelper: serviceLocator.get<ConnectionHelper>()),
+  );
+
+  serviceLocator.registerSingleton<AuthRepository>(
+    AuthRepositoryImpl(authDatasource: serviceLocator.get<AuthDatasource>()),
   );
 }
 
 void setupDomainServices() {
   serviceLocator.registerSingleton<NumberTranslatorDomainRepository>(
-    NumberTranslatorDomainRepositoryImpl(
-        repository: serviceLocator.get<NumberTranslatorRepository>()),
+    NumberTranslatorDomainRepositoryImpl(repository: serviceLocator.get<NumberTranslatorRepository>()),
   );
 
   serviceLocator.registerSingleton<NumberTranslatorService>(
     NumberTranslatorServiceImpl(
       repository: serviceLocator.get<NumberTranslatorDomainRepository>(),
     ),
+  );
+
+  serviceLocator.registerSingleton<AuthDomainRepository>(
+    AuthDomainRepositoryImpl(authRepository: serviceLocator.get<AuthRepository>()),
+  );
+
+  serviceLocator.registerSingleton<AuthService>(
+    AuthServiceImpl(repository: serviceLocator.get<AuthDomainRepository>()),
   );
 }
