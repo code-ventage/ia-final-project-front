ae2820ea74025de5710d3b3e76b30832cb9a5a04
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-22 21:24:06 -0400

    feat: implement auth user entity saving feature

--- lib/number_translator/domain/use_cases/auth/auth_service.dart
+++ lib/number_translator/domain/use_cases/auth/auth_service.dart
@@ -2,6 +2,10 @@ import '../../entities/user_entity.dart';
 import '../../repositories/auth/auth_domain_repository.dart';
 
 abstract class AuthService {
+  UserEntity? get();
+
+  void set(UserEntity userEntity);
+
   Future<bool> signUp(UserEntity userEntity);
 
   Future<bool> logIn(UserEntity userEntity);
@@ -10,6 +14,13 @@ abstract class AuthService {
 class AuthServiceImpl extends AuthService {
   final AuthDomainRepository repository;
   bool isSigned = false;
+  UserEntity? _userEntity;
+
+  @override
+  UserEntity? get() => _userEntity;
+
+  @override
+  void set(UserEntity userEntity) => _userEntity = userEntity;
 
   AuthServiceImpl({required this.repository});
 
