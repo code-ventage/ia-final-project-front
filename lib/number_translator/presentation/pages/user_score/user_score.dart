9ba772ae1b8b0a909e74d8da39a1d21a3362ea9c
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-25 16:47:00 -0400

    feat: implement some things on user_score.dart

--- lib/number_translator/presentation/pages/user_score/user_score.dart
+++ lib/number_translator/presentation/pages/user_score/user_score.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:ia_final_project_front/number_translator/domain/entities/user_score_entity.dart';
+import 'package:ia_final_project_front/number_translator/presentation/bloc/score/score_cubit.dart';
 
 import '../../widgets/custom_text_form_field_widget.dart';
 
@@ -19,6 +21,7 @@ class UserScorePage extends StatelessWidget {
       UserScoreEntity(username: 'pepe', score: '200'),
       UserScoreEntity(username: 'pepe', score: '200'),
     ];
+    //TODO 3/25/24 palmerodev : delete this
 
     return Scaffold(
       appBar: AppBar(
@@ -34,48 +37,36 @@ class UserScorePage extends StatelessWidget {
               width: MediaQuery.of(context).size.width * 0.9,
               child: CustomTextFormField(
                 suffix: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    //TODO 3/25/24 palmerodev : make the filter
-                  }
-                ),
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      //TODO 3/25/24 palmerodev : make the filter adn change the state
+                    }),
                 controller: TextEditingController(),
                 readOnly: false,
               ),
             ),
           ),
           const Gap(20),
-          SingleChildScrollView(child: Scores(scores: scores)),
+          BlocBuilder<ScoreCubit, ScoreState>(
+            builder: (context, state) {
+              return SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: ListView.builder(
+                    itemCount: scores.length, //TODO 3/25/24 palmerodev : get this from the cubit
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                          title: Text(scores[index].score, style: const TextStyle(fontSize: 20)),
+                          trailing: Text(scores[index].username, style: const TextStyle(fontSize: 20)),
+                          leading: const Icon(Icons.sports_score, size: 32));
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
         ],
-      ),
-    );
-  }
-}
-
-class Scores extends StatelessWidget {
-  const Scores({
-    super.key,
-    required this.scores,
-  });
-
-  final List<UserScoreEntity> scores;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.8,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: ListView.builder(
-          itemCount: scores.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(scores[index].score, style: const TextStyle(fontSize: 20)),
-              trailing: Text(scores[index].username, style: const TextStyle(fontSize: 20)),
-              leading: const Icon(Icons.sports_score, size: 32)
-            );
-          },
-        ),
       ),
     );
   }
